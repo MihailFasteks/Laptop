476186efdfe0fb7a0714eef7f01a3019c76710dd
MihailFasteks <fastakovskiy.mihail04@gmail.com>
2023-09-27 08:04:17 +0300

    Initial Commit

---
+++ Laptop/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Laptop
+//
+//  Created by Michalis on 27.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
